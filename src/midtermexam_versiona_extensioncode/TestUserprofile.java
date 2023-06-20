89dcf2d6d3e11babbd72c7cd879cdf42f00e1cdc
Jaskeerat Singh <sin14143@sheridancollege.ca>
2023-06-20 14:04:16 -0400

    Created a new repository, initialized it and added a new class called TestUserprofile

---
+++ src/midtermexam_versiona_extensioncode/TestUserprofile.java
@@ -0,0 +1,24 @@
+package midtermexam_versiona_extensioncode;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author jaske
+ */
+public class TestUserprofile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        UserProfile user1 = new UserProfile("123","Comedy");
+        
+        System.out.println("Your user profile was created ");
+    }
+    
+}
