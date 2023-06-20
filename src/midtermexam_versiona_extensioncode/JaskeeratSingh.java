e75ac98619cf497e24ff1e12d39b8faaf0a47657
Jaskeerat Singh <sin14143@sheridancollege.ca>
2023-06-20 14:09:41 -0400

    Created a new branch and a new class with commant in it.

---
+++ src/midtermexam_versiona_extensioncode/JaskeeratSingh.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author jaske
+ */
+public class JaskeeratSingh {
+    //created a new branch and a new class inside it.
+}
